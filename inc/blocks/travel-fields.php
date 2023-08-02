9b6bf07307211318311a49d43dd219401d665c80
Alex <skyriver.f1@gmail.com>
2023-08-03 03:17:19 +0500

    travel car, yacht rentals posts

--- inc/blocks/travel-fields.php
+++ inc/blocks/travel-fields.php
@@ -15,14 +15,6 @@ Container::make('post_meta', __( 'Информация о путешествии
   ->add_fields( array(
     Field::make('text', 'travel_title', __('Заголовок')),
     Field::make('text', 'travel_desc', __('Описание')),
-    Field::make('complex', 'travel_items', __('Список'))
-    ->setup_labels($elements_labels)
-    ->set_layout('tabbed-horizontal')
-    ->add_fields(array(
-      Field::make('image', 'img', __('Изображение'))->set_width(20),
-      Field::make('text', 'title_text', __('Текст над заголовком'))->set_width(40),
-      Field::make('text', 'title', __('Заголовок'))->set_width(40),
-    ))->set_collapsed(true)->set_header_template('<%- title %>'),
     Field::make('text', 'travel_bottom_title', __('Нижний заголовок')),
     Field::make('text', 'travel_bottom_desc', __('Нижние описание')),
 ));
--- template-parts/travel-block.php
+++ template-parts/travel-block.php
@@ -1,4 +1,11 @@
-<?php if ( !empty ($travel = carbon_get_post_meta($post->ID, 'travel_items') ) ) : ?>
+<?php
+  $car_id = 61;
+  $yacht_id = 63;
+
+  $car = get_term($car_id);
+  $yacht = get_term($yacht_id);
+
+?>
 <section class="travel container">
   <div class="travel__header">
     <h2 class="travel__title text-splitter">
@@ -9,22 +16,34 @@
     </div>
   </div>
   <div class="travel__items">
-
-    <?php foreach ($travel as $key => $item): ?>
-    <div class="travel__item rellax2" data-rellax-speed="<?php echo $key == 0 ? 6 : 2 ?>" data-rellax-percentage="0.46" data-rellax-min="0">
+    <div class="travel__item rellax2" data-rellax-speed="6" data-rellax-percentage="0.46" data-rellax-min="0">
       <div class="travel__item-image">
-        <img loading="lazy" width="920" height="669" sizes="100vw" src="<?php echo wp_get_attachment_image_url($item['img'], 'full'); ?>" alt="">
+        <img loading="lazy" width="920" height="669" sizes="100vw" src="<?php echo wp_get_attachment_image_url(carbon_get_term_meta($car_id, 'service_img'), 'full'); ?>" alt="">
       </div>
       <div class="travel__item-container">
         <div class="travel__item-text text-splitter">
-          <?php echo $item['title_text']; ?>
+          <?php echo $car->description ?>
         </div>
         <div class="travel__item-title text-splitter">
-          <?php echo $item['title']; ?>
+          <?php echo $car->name ?>
         </div>
       </div>
+      <a href="<?php echo get_term_link($car_id, $car->taxonomy); ?>" class="travel__item-link" aria-label="<?php echo $car->name ?>"></a>
     </div>
-    <?php endforeach; ?>
+    <div class="travel__item rellax2" data-rellax-speed="2" data-rellax-percentage="0.46" data-rellax-min="0">
+      <div class="travel__item-image">
+        <img loading="lazy" width="920" height="669" sizes="100vw" src="<?php echo wp_get_attachment_image_url(carbon_get_term_meta($yacht_id, 'service_img'), 'full'); ?>" alt="">
+      </div>
+      <div class="travel__item-container">
+        <div class="travel__item-text text-splitter">
+          <?php echo $yacht->description ?>
+        </div>
+        <div class="travel__item-title text-splitter">
+          <?php echo $yacht->name ?>
+        </div>
+      </div>
+      <a href="<?php echo get_term_link($yacht_id, $yacht->taxonomy); ?>" class="travel__item-link" aria-label="<?php echo $yacht->name ?>"></a>
+    </div>
   </div>
   <div class="travel__footer">
     <div class="travel__footer-title text-splitter">
@@ -35,4 +54,3 @@
     </div>
   </div>
 </section><!-- travel -->
-<?php endif; ?>
